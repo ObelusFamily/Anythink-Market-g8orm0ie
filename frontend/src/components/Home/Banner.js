7f043b57d057a286314d5b0f3196db2d6dd7ef05
skyreal <102043619+Skywonda@users.noreply.github.com>
2022-12-07 23:26:45 +0100

    Revert "enabled title filter on frontend"

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,15 +1,7 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
-import agent from "../../agent";
 
-const Banner = (props) => {
-  const onSearchChange = (event) => {
-    props.onSearchFilter(
-      event.target.value,
-      (page) => agent.Items.byTitle(event.target.value),
-      agent.Items.byTitle(event.target.value)
-    );
-  };
+const Banner = () => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
@@ -17,14 +9,6 @@ const Banner = (props) => {
         <div>
           <span>A place to </span>
           <span id="get-part">get</span>
-          <form>
-            <input
-              className="input-group"
-              placeholder="What is it that you truly desire?"
-              type="text"
-              onChange={onSearchChange}
-            />
-          </form>
           <span> the cool stuff.</span>
         </div>
       </div>
