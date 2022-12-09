28ffea9657fc44018a49babe9a2b06aa3ff84db9
skyreal <102043619+Skywonda@users.noreply.github.com>
2022-12-09 18:55:16 +0100

    Revert "scolding text"

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -7,14 +7,6 @@ const ItemList = (props) => {
     return <div className="py-4">Loading...</div>;
   }
 
-  if (props.noResult) {
-    return (
-      <div className="container" id="empty">
-        No item found
-      </div>
-    );
-  }
-
   if (props.items.length === 0) {
     return <div className="py-4 no-items">No items are here... yet.</div>;
   }
