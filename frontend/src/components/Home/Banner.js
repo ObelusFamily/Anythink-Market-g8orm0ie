9949996cd46806ca883e3906bcee16fb9fc7a8cf
Skywonda <princesam3600@gmail.com>
2022-12-11 18:16:13 +0100

    search box ui

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,7 +1,15 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import agent from "../../agent";
 
-const Banner = () => {
+const Banner = (props) => {
+  const onSearchChange = (e) => {
+    props.onSearchFilter(
+      e.target.value,
+      (page) => agent.Items.byTitle(e.target.value, page),
+      agent.Items.byTitle(e.target.value)
+    );
+  };
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
@@ -9,6 +17,13 @@ const Banner = () => {
         <div>
           <span>A place to </span>
           <span id="get-part">get</span>
+          <input
+            className="field"
+            style={{ margin: "10px" }}
+            placeholder="What is it that you truly desire?"
+            id="search-box"
+            onChange={onSearchChange}
+          />
           <span> the cool stuff.</span>
         </div>
       </div>
