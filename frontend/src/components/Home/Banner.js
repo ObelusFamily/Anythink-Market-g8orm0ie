bb382b88f4627c4beb58368afff6d24950b7add0
Skywonda <princesam3600@gmail.com>
2022-12-15 19:41:17 +0100

    search visibility

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import agent from "../../agent";
 
@@ -10,20 +10,30 @@ const Banner = (props) => {
       agent.Items.byTitle(e.target.value)
     );
   };
+  const [searchVisible, setSearchVisible] = useState(false);
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span>A place to </span>
-          <span id="get-part">get</span>
-          <input
-            className="field"
-            style={{ margin: "10px" }}
-            placeholder="What is it that you truly desire?"
-            id="search-box"
-            onChange={onSearchChange}
-          />
+          <span
+            id="get-part"
+            onClick={() => {
+              setSearchVisible(true);
+            }}
+          >
+            get
+          </span>
+          {searchVisible && (
+            <input
+              className="field"
+              style={{ margin: "10px" }}
+              placeholder="What is it that you truly desire?"
+              id="search-box"
+              onChange={onSearchChange}
+            />
+          )}
           <span> the cool stuff.</span>
         </div>
       </div>
