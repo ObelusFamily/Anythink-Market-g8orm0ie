822e036cc3e1eb4c5c4eed5416d32e56c1333a67
Skywonda <princesam3600@gmail.com>
2022-12-15 14:36:47 +0100

    scolding text

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -7,12 +7,16 @@ const ItemList = (props) => {
     return <div className="py-4">Loading...</div>;
   }
 
-  if (props.items.length === 0) {
+  if (props.noResult) {
     return (
-      <div className="py-4 no-items" id="empty">
-        No items are here... yet.
+      <div className="container" id="empty">
+        No item here yet bro
       </div>
     );
+  }
+
+  if (props.items.length === 0) {
+    return <div className="py-4 no-items">No items are here... yet.</div>;
   }
 
   return (
